403ec52a956c7da09483f1e45e7dee78a00ed2fc
TRussell <TRussell@MATH-2ED52D-WIN>
2016-09-28 14:34:48 -0500

    Add project files.

---
+++ BinaryDecisionForest/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// BinaryDecisionForest.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
